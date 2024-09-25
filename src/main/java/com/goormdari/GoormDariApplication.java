394615c94ae44f310e68a757f4c6ad594b5bb239
LEEJaeHyeok97 <hazardous10@naver.com>
2024-09-25 11:18:17 +0900

    Initial commit

---
+++ src/main/java/com/goormdari/GoormDariApplication.java
@@ -0,0 +1,13 @@
+package com.goormdari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormDariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoormDariApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goormdari/GoormDariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormdari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormDariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
