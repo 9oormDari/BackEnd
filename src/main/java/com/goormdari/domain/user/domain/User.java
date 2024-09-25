9681c43289ba43077fe697d93a2dd771753d4cd0
이재혁 <hazardous10@naver.com>
2024-09-25 15:51:51 +0900

    chore: ::예외처리, 전역 환경 설정 외 (#2)
    * chore: timezone seoul 설정
    
    * chore: 깃 템플릿 추가, readme.md 추가
    
    * chore: 예외처리, 전역 환경 설정 외

---
+++ src/main/java/com/goormdari/domain/user/domain/User.java
@@ -0,0 +1,17 @@
+package com.goormdari.domain.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+}
