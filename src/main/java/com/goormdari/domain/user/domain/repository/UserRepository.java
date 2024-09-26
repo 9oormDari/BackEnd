0266b720a3360437cdef5b9d7d4c44fc93c554a4
이재혁 <hazardous10@naver.com>
2024-09-26 14:03:25 +0900

    chore: 디스코드 webhook 테스트 (#6)
    * chore: timezone seoul 설정
    
    * chore: 깃 템플릿 추가, readme.md 추가
    
    * chore: 예외처리, 전역 환경 설정 외
    
    * feat: 데이터 모델링
    
    * chore: :whale: cicd 설정
    
    * chore: :whale: workflow lowercase 로 수정
    
    * chore: 디스코드 webhook 테스트

---
+++ src/main/java/com/goormdari/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.goormdari.domain.user.domain.repository;
+
+import com.goormdari.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
