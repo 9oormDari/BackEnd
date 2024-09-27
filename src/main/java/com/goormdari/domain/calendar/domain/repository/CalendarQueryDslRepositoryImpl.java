b36ca9464707f065371631c1f60faf9b5a916e1f
이재혁 <hazardous10@naver.com>
2024-09-28 03:11:04 +0900

    feat: :sparkles: 루틴 달성률 추가 (#23)

--- src/main/java/com/goormdari/domain/calendar/domain/repository/CalendarQueryDslRepositoryImpl.java
+++ src/main/java/com/goormdari/domain/calendar/domain/repository/CalendarQueryDslRepositoryImpl.java
@@ -2,13 +2,16 @@ package com.goormdari.domain.calendar.domain.repository;
 
 import com.goormdari.domain.calendar.domain.Calendar;
 import com.goormdari.domain.calendar.dto.response.CheckGoalProgressResponse;
+import com.goormdari.domain.team.domain.QTeam;
+import com.goormdari.domain.team.domain.Team;
+import com.goormdari.domain.user.domain.QUser;
 import com.goormdari.domain.user.domain.User;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -60,13 +63,52 @@ public class CalendarQueryDslRepositoryImpl implements CalendarQueryDslRepositor
                 ))
                 .collect(Collectors.toList());
 
+        Double routineCompletionRate = calculateRoutineCompletionRate(userId, date);
+
         return new CheckGoalProgressResponse(
                 userInfo.getNickname(),
                 userInfo.getProfileUrl(),
                 goal,
                 dDay,
-                dayAchiveList
+                dayAchiveList,
+                routineCompletionRate
         );
+    }
+
+    private Double calculateRoutineCompletionRate(Long userId, YearMonth date) {
+        User user = queryFactory
+                .selectFrom(QUser.user)
+                .where(QUser.user.id.eq(userId))
+                .fetchOne();
+        Long teamId = user.getTeam().getId();
+
+        Team team = queryFactory
+                .selectFrom(QTeam.team)
+                .where(QTeam.team.id.eq(teamId))
+                .fetchOne();
+
+        LocalDateTime createdAt = team.getCreatedAt();
+
+        LocalDate deadLine = team.getDeadLine();
+        LocalDate createdAtDate = createdAt.toLocalDate();
+
+        // 일 수 차이 계산
+        Long daysBetween = ChronoUnit.DAYS.between(createdAtDate, deadLine);
+
+        // 루틴 4개 수행한 날짜 계산
+        Long successCount = queryFactory
+                .select(calendar.count())
+                .from(calendar)
+                .where(calendar.userId.eq(userId),
+                        calendar.success_count.eq(4)
+                )
+                .fetchFirst();
+
+        Double rate = ((double) (successCount) / (double) (daysBetween)) * 100;
+
+        double roundedRate = Math.round(rate * 10) / 10.0;
+
+        return roundedRate;
     }
 
     private int calculateDDay(LocalDate deadLine) {
